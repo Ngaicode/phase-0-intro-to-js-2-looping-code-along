d5a3bd9b2de9b28e31a7efb5524e3f7c2030c470
Ngaicode <mjombakevin376@gmail.com>
2022-06-09 16:49:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+"use strict";
+let meow = ["wolfie", "susan", "mercy"];
+function writeCards(arrayOfNames, event) {
+  let thankYouCards = [];
+  for (let i = 0; i < arrayOfNames.length; i++) {
+    thankYouCards.push(
+      `Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouCards;
+}
+writeCards(meow, "birthday");
+function countDown(startNum) {
+  while (startNum > 0) {
+    console.log(startNum);
+    startNum -= 1;
+  }
+  console.log(startNum);
+}
+countDown(11);
